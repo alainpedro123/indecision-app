97a1eee5e852583b6b0e3a55f4b39bed20a45f9a
Alain Afonso <alainpedro123@gmail.com>
2019-05-25 15:25:28 +0200

    Updates

---
+++ precache-manifest.55664cb172225213933bacc2d31a57ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d310b08082374dd4cdd67d56aaa8a28d",
+    "url": "/indecision-app/index.html"
+  },
+  {
+    "revision": "b305bf219c1fc9a0bc17",
+    "url": "/indecision-app/static/css/main.b3b2c6aa.chunk.css"
+  },
+  {
+    "revision": "0cad985d3754358d12a3",
+    "url": "/indecision-app/static/js/2.3ad5b862.chunk.js"
+  },
+  {
+    "revision": "b305bf219c1fc9a0bc17",
+    "url": "/indecision-app/static/js/main.7ee7d56e.chunk.js"
+  },
+  {
+    "revision": "22abca185530f4668210",
+    "url": "/indecision-app/static/js/runtime~main.0b638552.js"
+  }
+]);
